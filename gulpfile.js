88add930496693f92b744907b3aec3ad54eb1d51
Pierre de Leastar <pdeleastar@gmail.com>
2016-04-10 17:02:23 +0100

    added bower package

---
+++ gulpfile.js
@@ -0,0 +1,11 @@
+var gulp = require('gulp'),
+  connect = require('gulp-connect');
+
+gulp.task('connect', function () {
+  connect.server({
+    root: 'app/',
+    port: 8888
+  });
+});
+
+gulp.task('default', ['connect']);
